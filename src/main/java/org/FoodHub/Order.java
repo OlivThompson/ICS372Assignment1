8bd12d5c0eb46e2ab3cb4d7a93276d55d30f4692
Scener3 <phengkvang.mn@gmail.com>
2025-09-23 18:52:36 -0500

    Added getOrderFromJSON() fetching data from JSON file into Order foodItems Array

--- src/main/java/org/FoodHub/Order.java
+++ src/main/java/org/FoodHub/Order.java
@@ -6,31 +6,49 @@ import java.util.ArrayList;
 public class Order {
 
     private int orderId;
-    private ArrayList<FoodItem> foodItems = new ArrayList<>();
+    private ArrayList<foodItem> foodItems = new ArrayList<foodItem>();
     private String orderStatus;
     private LocalDateTime orderTime;
-    private String jsonOrderData;
 
-    public Order(int orderId, ArrayList <FoodItem> foodItems, String orderStatus,
-                 LocalDateTime orderTime, String jsonOrderData) {
+//    public Order(int orderId, ArrayList <foodItem> foodItems, String orderStatus,
+//                 LocalDateTime orderTime) {
+//        this.orderId = orderId;
+//        this.foodItems = foodItems;
+//        this.orderStatus = orderStatus;
+//        this.orderTime = orderTime;
+//    }
+
+    public Order(int orderId){
         this.orderId = orderId;
-        this.foodItems = foodItems;
-        this.orderStatus = orderStatus;
-        this.orderTime = orderTime;
-        this.jsonOrderData = jsonOrderData;
+    }
+
+    /// WHAT DO I DO WITH THE QUANTITY, HOW SHOULD I IMPLEMENT IT?
+    /// 1. Parameter 1 adds the food into the foodItems array
+    /// 2. Quantity.....How should I calculate it...need it to access price and * it
+    /// Where should I store quantity?
+    /// Maybe...I add quantity variable to foodItem and then use setQuantity to the item with parameter 2
+//    public void orderAddItem(foodItem item, int quantity){
+//        foodItems.add(item);
+//        int itemIndex = foodItems.indexOf(item);
+//        foodItems.get(itemIndex).setFoodQuantity(quantity);
+//    }
+
+    public void getOrderItemFromJSON(ArrayList<foodItem> foodItem){
+        this.foodItems = foodItem;
     }
 
     public int getOrderId(){
         return orderId;
     }
 
-    public double getTotalPrice(){
-        double totalPrice = 0;
-        for (Object foodItem : foodItems) {
-            totalPrice += foodItem.getPrice();
-        }
-        return totalPrice;
-    }
+//    public double getTotalPrice(){
+//        double totalPrice = 0;
+//        for (Object foodItem : foodItems) {
+//            totalPrice += foodItem.getPrice();
+//        }
+//        return totalPrice;
+//    }
+
     public int getStatus(){
         try {
             return Integer.parseInt(orderStatus);
@@ -39,8 +57,8 @@ public class Order {
         }
     }
 
-    public String getJson(){
-        return  jsonOrderData;
+    public void printOrderItems(){
+        System.out.println(foodItems);
     }
 
 }
