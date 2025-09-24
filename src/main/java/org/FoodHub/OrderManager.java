dfb67ef0a602cc4b4c288e5ab9c66c809405f4a0
Scener3 <phengkvang.mn@gmail.com>
2025-09-24 13:19:36 -0500

    Implemented some functionality to the functions in OrderManager class

--- src/main/java/org/FoodHub/OrderManager.java
+++ src/main/java/org/FoodHub/OrderManager.java
@@ -4,31 +4,66 @@ import java.util.List;
 import java.util.Queue;
 
 public class OrderManager {
-    private Queue<Order> orders = new Queue<Order>();
+    private ArrayList<Order> allOrders = new ArrayList<Order>();
+    private static int orderID = 0;
+    private String orderType;
+    private long orderDate;
+    int status = 0;  /// By default
+    /// 0 = Incomplete/Start
+    /// 1 = Complete
+    /// 2 = Cancelled
+    parseOrder orderParser = new parseOrder();
 
-    private List<Order> completedOrders = new ArrayList<Order>();
 
-    public void addOrder(Order order) {
-
+    public OrderManager(){
+        orderType = orderParser.getOrderType();
+        orderDate = orderParser.getOrderTime();
     }
 
-    public void cancelOrder(int orderID) {
+//    public void addOrder(Order order) {
+//        allOrders.add(order);
+//    }
 
+    public void cancelOrder(Order order) {
+        order.setStatus(2);
+        allOrders.remove(order);
     }
 
     public void startIncomingOrder() {
-
+        orderID += 1;
+        Order newOrder = new Order(orderID, orderType, orderDate, status);
+        allOrders.add(newOrder);
     }
 
-    public void completeIncomingOrder(int orderID) {
+    public ArrayList<Order> getCompletedOrders() {
+        ArrayList<Order> completedOrders = new ArrayList<Order>();
+        for (Order theOrder : allOrders){
+            if (theOrder.getStatus() == 1){
+                completedOrders.add(theOrder);
+            }
+        }
+        return completedOrders;
+    }
 
+    public ArrayList<Order> getIncompleteOrders() {
+        ArrayList<Order> incompleteOrders = new ArrayList<Order>();
+        for (Order theOrder : allOrders){
+            if (theOrder.getStatus() == 0){
+                incompleteOrders.add(theOrder);
+            }
+        }
+        return incompleteOrders;
     }
 
     public void exportAllOrders() {
-
+        orderParser.writeAllOrderToFile(allOrders);
     }
 
-    public List<Order> getIncompleteOrders() {
-        return null;
+    public ArrayList<Order> getAllOrders(){
+        return allOrders;
+    }
+
+    public void printIncomingOrder(){
+        orderParser.readOrdersFromJson();
     }
 }
