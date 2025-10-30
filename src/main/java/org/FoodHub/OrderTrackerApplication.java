8c969369c4473d5cf73f3129d7fa711366cd77fd
Sterling-G <re4816ku@go.minnstate.edu>
2025-10-30 18:26:54 -0500

    Added launcher, application, controller, and xml files for UI.

---
+++ src/main/java/org/FoodHub/OrderTrackerApplication.java
@@ -0,0 +1,20 @@
+package org.FoodHub;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class OrderTrackerApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("OrderTrackerView.fxml")));
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Restaurant Order Manager");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/org/FoodHub/OrderTrackerController.java
@@ -0,0 +1,25 @@
+package org.FoodHub;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class OrderTrackerController {
+    public TableView orderTable;
+    public TableColumn idColumn;
+    public TableColumn typeColumn;
+    public TableColumn statusColumn;
+    public ComboBox statusBox;
+    public Button updateButton;
+    public Button exportButton;
+
+    public void handleUpdateStatus(ActionEvent actionEvent) {
+
+    }
+
+    public void handleExportOrders(ActionEvent actionEvent) {
+
+    }
+}
