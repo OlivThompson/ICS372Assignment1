d57d42c8bb08b352746c4f1a66c21f41433103e1
Sterling-G <re4816ku@go.minnstate.edu>
2025-11-03 21:38:46 -0600

    Added more basic tests, changed grammar of some requirements.

--- docs/requirements.md
+++ docs/requirements.md
@@ -4,12 +4,12 @@
 ### 4. The software shall be able to update visually to the UI in real time while orders are being put into the "orders" directory
 ### 5. When setting the order status to "completed" and click "update status", the software shall automatically export the order into the "orders/completedOrder" directory
 ### 6. When selecting an order there shall be a button to display order and when clicked the software shall present all the order's details
-### 7. In each order type, the software shall have a logo to represent their order type for pick up, delivery, togo
+### 7. In each order type, the software shall have a logo to represent their order type for pick-up, delivery, togo
 ### 8. When an order is being processed with no date, the software shall create order_date at the moment it is being processed
-### 9. If SavedDataForLoad.json file already exists but is completely empty, the software shall still be able to startup the program and load all the orders that still needs to be processed
+### 9. If SavedDataForLoad.json file already exists but is completely empty, the software shall still be able to start up the program and load all the orders that still needs to be processed
 ### 10. The software shall have a button that will export all orders called "Export Orders", and shall export all current processed orders into "orders/all_orders" directory
 ### 11. The software shall ignore order file "orderID" if one exists and create import it into the next available orderID of the manager
-### 12. The software shall only allow order type "delivery" to be able to use the "Delivery Status" bar and update it's status
+### 12. The software shall only allow order type "delivery" to be able to use the "Delivery Status" bar and update its status
 ### 13. The software shall have four status for the order, "Incoming", "Started", "Completed", "Cancelled" and allowed to be updated at runtime and display to the UI
 ### 14. The delivery status for the orders shall have two status, "Pending", "Out For Delivery" and allowed to be updated only for delivery type orders and displayed to the UI
 ### 15. The software shall display all available orders onto the each columns of their respected information
--- src/test/java/org/FoodHub/Tester.java
+++ src/test/java/org/FoodHub/Tester.java
@@ -1,9 +1,7 @@
 package org.FoodHub;
 
 import org.junit.Assert.*;
-import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -20,6 +18,21 @@ public class Tester {
     }
 
     @Test
+    void testOrderCreationAndAttributes() {
+        FoodItem item1 = new FoodItem("Brisket", 2, 50.0);
+        FoodItem item2 = new FoodItem("Macaroni", 1, 4.0);
+
+        Order order = new Order(List.of(item1, item2), OrderStatus.INCOMING, System.currentTimeMillis(), OrderType.DELIVERY);
+
+        Assertions.assertEquals(2, order.getFoodItems().size());
+        Assertions.assertEquals(OrderStatus.INCOMING, order.getOrderStatus());
+        Assertions.assertEquals(OrderType.DELIVERY, order.getOrderType());
+        Assertions.assertEquals(104.0, order.calculateTotalPrice());
+        Assertions.assertNotNull(order.getDeliveryStatus());
+        Assertions.assertEquals(OrderTypeIcon.Delivery_Icon, order.getOrderTypeIcon());
+    }
+
+    @Test
     void testSettingOrderAndDeliveryStatuses() {
         Order order = new Order(List.of(), OrderStatus.INCOMING, System.currentTimeMillis(), OrderType.DELIVERY);
         order.setOrderStatus(OrderStatus.STARTED);
@@ -27,10 +40,27 @@ public class Tester {
 
         Assertions.assertEquals(OrderStatus.STARTED, order.getOrderStatus());
         Assertions.assertEquals(DeliveryStatus.Out_For_Delivery, order.getDeliveryStatus());
+
+        order.setOrderStatus(OrderStatus.COMPLETED);
+
+        Assertions.assertEquals(OrderStatus.COMPLETED, order.getOrderStatus());
+
+        order.setOrderStatus(OrderStatus.CANCELLED);
+
+        Assertions.assertEquals(OrderStatus.CANCELLED, order.getOrderStatus());
     }
 
     @Test
-    void testGettingExtensions() {
+    void testOrderManager() {
+        OrderManager om = new OrderManager();
+        Order order = new Order(List.of(new FoodItem("Pancakes", 100, 5)), OrderStatus.INCOMING, System.currentTimeMillis(), OrderType.TOGO);
+        om.addOrder(order);
+
+        Assertions.assertEquals(order, om.findOrder(order.getOrderID()));
+    }
+
+    @Test
+    void testFileAccessor() {
         FileAccesser accesser = new FileAccesser();
 
         Assertions.assertEquals("json", accesser.getExtension("file.json"));
